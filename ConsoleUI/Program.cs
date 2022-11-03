40fa2aafa22c60bc383a2bc9e592ff103e7238a1
MichealLeeCoding <MichealLeeCoding@gmail.com>
2022-11-03 11:45:38 +0700

    T1

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,38 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static ShoppingCartModel cart = new ShoppingCartModel();
+
+        static void Main(string[] args)
+        {
+            PopulateCartWithDemoData();
+
+            Console.WriteLine($"The total for the cart is {cart.GenerateTotal(SubTotalAlert):C2}"); //Write like this can allow us turn the number to dollar
+            
+
+            Console.WriteLine();
+            Console.Write("Please press any key to exit the application...");
+            Console.ReadKey();
+        }
+        private static void SubTotalAlert(decimal Subtotal)
+        {
+            Console.WriteLine($"The subtotal is {Subtotal:C2}");
+        }
+        private static void PopulateCartWithDemoData()
+        {
+            cart.Items.Add(new ProductModel { ItemName = "Cereal", Price = 3.63M });
+            cart.Items.Add(new ProductModel { ItemName = "Milk", Price = 2.95M });
+            cart.Items.Add(new ProductModel { ItemName = "Strawberries", Price = 7.51M });
+            cart.Items.Add(new ProductModel { ItemName = "Blueberries", Price = 8.84M });
+        }
+    }
+}
